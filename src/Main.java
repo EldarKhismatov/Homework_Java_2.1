d2e2b8fb631ea1b624676c22f4c7dc6afba1b5aa
Eldar <ekhismatov@list.ru>
2023-06-22 09:45:22 +0300

    Homework

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int value = 13676;
+        int valueMill = 20;
+
+        int bonusMill = value / valueMill;
+        System.out.print("Итого бонусов насчитано: " + bonusMill);
+    }
+}
